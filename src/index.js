3bfa2e9bb0c65f8223c90383d2d432b503f9520c
saloni2811 <saloni28singh2001@gmail.com>
2023-04-01 10:44:59 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const name = "Saloni";
+const luckyNumber = 14;
 
-
-ReactDom.render(<h1>Hello {name}!</h1> , document.getElementById("root"));
+ReactDom.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>My lucky number is {luckyNumber}.</p> 
+</div>,
+ document.getElementById("root"));
